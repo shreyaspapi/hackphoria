f5f781b30b8e33445bef7db16976e06ef79dabca
Tunap <tunappaul@gmail.com>
2020-02-15 14:55:54 +0530

    add torus-integration

---
+++ torus-integration/build/precache-manifest.8d5eb3e9288653ebded48f81073c2c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4aca721f3c3aac655017f4fd84dc0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20997d2e0c71e91ea660",
+    "url": "/static/css/main.c85840c7.chunk.css"
+  },
+  {
+    "revision": "cbcd4a334ea618e044f1",
+    "url": "/static/js/2.67708853.chunk.js"
+  },
+  {
+    "revision": "ff1dbff6f629d807cc23827d95667a81",
+    "url": "/static/js/2.67708853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20997d2e0c71e91ea660",
+    "url": "/static/js/main.763b6cf5.chunk.js"
+  },
+  {
+    "revision": "ad56bd809843d1e0726e",
+    "url": "/static/js/runtime-main.f5ea2767.js"
+  }
+]);
